7f74e98a613872a91706dbc1e3adc791d8427f58
Ziv <ziv.perry@zoominfo.com>
2022-12-26 10:48:43 +0200

    bar

--- src/foo.test.ts
+++ src/foo.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { foo } from './foo';
+import { foo, bar } from './foo';
 
 describe('test the foo() function', () => {
 
@@ -23,6 +23,22 @@ describe('test the foo() function', () => {
         it('should return 24 for input of 11, 13', () => {
             const ret = foo(11, 13);
             expect(ret).to.equal(24);
+        });
+    });
+
+    describe('Another mighty function bar()', () => {
+        it('should return 1 for inputs 1,1', () => {
+            const ret = bar(1, 1);
+            expect(ret).to.equal(1);
+        });
+
+        it('should return 2 for inputs 4,2', () => {
+            const ret = bar(4, 2);
+            expect(ret).to.equal(2);
+        });
+
+        it('should throw an error for input 1, 0', () => {
+            // bar(1, 0);
         });
     });
 });
--- src/foo.ts
+++ src/foo.ts
@@ -1,3 +1,7 @@
 export function foo(a: number, b: number) {
     return a + b;
 }
+
+export function bar(a: number, b: number) {
+
+}
