4a2eaaf4b185c4833e7b69ebf8aede75d497407c
Ziv <ziv.perry@zoominfo.com>
2023-01-17 12:40:43 +0200

    typo

--- src/auth/auth-middleware.ts
+++ src/auth/auth-middleware.ts
@@ -4,15 +4,15 @@ import { verifyJWT } from '../services/auth-service';
 export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.token as string;
     if (!token) {
-        next(new Error('you shell not pass!!!'));
+        next(new Error('you shall not pass!!!'));
     }
 
     try {
         const {email} = verifyJWT<{email: string}>(token);
-        res.locals.email = email;
+        // req.user = email;
         next();
 
     } catch (e) {
-        next(new Error('you shell not pass!!!'));
+        next(new Error('you shall not pass!!!'));
     }
 }
